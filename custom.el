fbeeb3704f6c83c8fa4880c31165c6416153fd2e
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2017-05-13 23:53:53 +0100

    Uninstalled couple of useless themes

--- custom.el
+++ custom.el
@@ -70,7 +70,7 @@
  '(org-support-shift-select t)
  '(package-selected-packages
    (quote
-    (ox-reveal ox-impress-js org-impress-js ox-gfm org-brain company-ispell zenburn-theme atom-one-dark-theme helm-org-rifle beacon hl-todo imenu-anywhere webpaste dumb-jump pomidor company-yasnippet eclimd evil-lion git-gutter-fringe git-timemachine lice evil-goggles evil-org dired-launch palette auto-yasnippet evil-mc ace-jump-helm-line use-package esup quickrun flx evil-magit nlinum nlinum-relative evil-god-state god-mode evil-args evil-visualstar evil-matchit evil-anzu evil-surround evil-leader linum-relative powerline-evil powerline htmlize latex-extra tabbar rainbow-mode rainbow-delimiters org-bullets helm-swoop company-web company-jedi auto-package-update auto-highlight-symbol anaphora ac-html-bootstrap ac-html ac-dabbrev undo-tree)))
+    (ox-reveal ox-impress-js org-impress-js ox-gfm org-brain company-ispell atom-one-dark-theme beacon hl-todo imenu-anywhere pomidor company-yasnippet eclimd evil-lion git-gutter-fringe git-timemachine lice dired-launch palette auto-yasnippet ace-jump-helm-line quickrun flx evil-magit nlinum nlinum-relative evil-god-state god-mode evil-args evil-visualstar evil-matchit evil-anzu evil-surround evil-leader powerline-evil powerline htmlize latex-extra tabbar rainbow-mode rainbow-delimiters org-bullets helm-swoop company-web company-jedi auto-package-update auto-highlight-symbol anaphora ac-html-bootstrap ac-html ac-dabbrev undo-tree)))
  '(palette-hex-rgb-digits 2)
  '(powerline-gui-use-vcs-glyph nil)
  '(python-shell-completion-native-disabled-interpreters (quote ("pypy" "python")))
