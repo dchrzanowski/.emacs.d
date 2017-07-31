103c2e978b54bfdbb4db9ab98eca5f3cdd4927de
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2017-07-31 15:56:45 +0100

    Fixed web-mode ejs hook

--- init.el
+++ init.el
@@ -542,6 +542,7 @@
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+
   ;; hooks
   (defun my-web-mode-hook ()
     "Hooks for Web mode."
@@ -562,10 +563,6 @@
             (lambda ()
               (when (string-equal "tsx" (file-name-extension buffer-file-name))
                 (setup-tide-mode))))
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (when (string-equal "ejs" (file-name-extension buffer-file-name))
-                (web-mode))))
 
   (add-hook 'web-mode-hook  'my-web-mode-hook))
 
