3df461c9ce800073b8fa66c1cebebbdcebc94210
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2017-10-11 11:04:37 +0100

    Coloured default font to a "nicer" white

--- custom.el
+++ custom.el
@@ -121,7 +121,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "PfEd" :family "DejaVu Sans Mono" :foreground "#DDDDDD"))))
  '(ahs-definition-face ((t (:background "black" :weight bold))))
  '(ahs-edit-mode-face ((t (:background "black" :foreground "dark orange"))))
  '(ahs-face ((t (:background "black"))))
