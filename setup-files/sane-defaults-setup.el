a4e3e444b9fb6e583b27d225f171b45d08f2d134
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2018-04-10 07:27:16 +0100

    Added default frame size

--- setup-files/sane-defaults-setup.el
+++ setup-files/sane-defaults-setup.el
@@ -104,5 +104,11 @@
 ;; --------------------------------------------------------------------
 (setq alert-default-style 'libnotify)
 
+;; --------------------------------------------------------------------
+;; new frame size
+;; --------------------------------------------------------------------
+(add-to-list 'default-frame-alist '(height . 60))
+(add-to-list 'default-frame-alist '(width . 160))
+
 (provide 'sane-defaults-setup)
 ;;; sane-defaults-setup.el ends here
