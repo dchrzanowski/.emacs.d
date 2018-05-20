4ebffc31fb8930dfe6d9bb318fc6d07824c640a7
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2018-05-20 21:55:52 +0100

    Requiring all the modules of docker

--- setup-files/docker-setup.el
+++ setup-files/docker-setup.el
@@ -13,7 +13,14 @@
 
 (use-package docker
   :config
-  (docker-global-mode))
+  (docker-global-mode)
+  (require 'docker-images)
+  (require 'docker-containers)
+  (require 'docker-machine)
+  (require 'docker-networks)
+  (require 'docker-process)
+  (require 'docker-utils)
+  (require 'docker-volumes))
 
 (use-package yaml-mode
   :mode ("\\.yml\\'" . yaml-mode))
