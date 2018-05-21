3518bb8854265329f554ecf3d2ac2314a409553d
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2018-05-21 11:30:52 +0100

    Reverted no standard docker setup

--- setup-files/docker-setup.el
+++ setup-files/docker-setup.el
@@ -13,14 +13,7 @@
 
 (use-package docker
   :config
-  (docker-global-mode)
-  (require 'docker-images)
-  (require 'docker-containers)
-  (require 'docker-machine)
-  (require 'docker-networks)
-  (require 'docker-process)
-  (require 'docker-utils)
-  (require 'docker-volumes))
+  (docker-global-mode))
 
 (use-package yaml-mode
   :mode ("\\.yml\\'" . yaml-mode))
