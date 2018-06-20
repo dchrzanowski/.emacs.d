d23d077988195eb5d735697e69114fbaf336117b
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2018-06-20 15:02:49 +0100

    Corrected hydra window operations

--- setup-files/hydra-setup.el
+++ setup-files/hydra-setup.el
@@ -15,7 +15,7 @@
 (defhydra hydra-window-operations (:color pink :hint nil)
   "
 _h_/_j_/_k_/_l_ move     _w_/_r_/_f_/_F_ buf/bkm/proj/file   _x_/_X_/_o_ delete ace/here/other    _n_/_p_ tabs          _D_ired kill all
-_H_/_J_/_K_/_L_ resize   _b_/_v_/_s2_/_s3_/_s4_ splits         _u_/_U_ undo/redo                  _0_-_9_ workspaces
+_H_/_J_/_K_/_L_ resize   _b_/_v_/_s2_/_s3_/_s4_ splits         _u_/_U_ undo/redo                  _0_-_9_ workspaces    _S_plit toggle
 _d_ ace            _W_ kill buffer               _i_/_I_ jump other                 _M-u_ clean
 _z_ swap           _=_ balance                   _q_uit                           _R_evert buffer
 =
