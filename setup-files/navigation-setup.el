b76a8e2eea3381b0e8527adf345f49528dc09791
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2018-08-07 21:48:35 +0100

    Temp removal of dired-icons

--- setup-files/navigation-setup.el
+++ setup-files/navigation-setup.el
@@ -153,9 +153,9 @@
 ;; all the icons
 ;; --------------------------------------------------------------------
 (use-package all-the-icons)
-(use-package all-the-icons-dired
-  :config
-  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+;; (use-package all-the-icons-dired
+;;   :config
+;;   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
 
 ;; --------------------------------------------------------------------
 ;; ibuffer
