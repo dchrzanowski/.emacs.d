8f8f00951f398591071163339ff2d357b9d21ff1
Damian Chrzanowski <pjdamian.chrzanowski@gmail.com>
2018-08-12 18:01:35 +0100

    added keychain-environment

--- custom.el
+++ custom.el
@@ -100,7 +100,7 @@
  '(org-support-shift-select t)
  '(package-selected-packages
    (quote
-    (company-tern tern kv a magit-todos yaml-mode docker dockerfile-mode moody minions multifiles define-word smart-jump highlight-thing speeddating use-package-ensure-system-package zerodark-theme ranger vlf vlfi org-super-agenda org-kanban org-edna gitattributes-mode gitconfig-mode dired-subtree dired-filter dired-ranger helm-google helm-gitignore helm-flx flx fzf ibuffer-vc helpful color-identifiers-mode darkroom org-notify org-alert toc-org org-projectile org-download diredful direful dired-hide-dotfiles evil-collection evil-colletion general company-statistics json-mode jedi virtualenv holiday holidays pdf-tools calfw-org calfw zenity-color-picker helm-system-packages exato evil-indent-plus evil-ediff evil-numbers evil-number evil-exchange ace-link git-messenger omnisharp omnisharp-emacs markdown-mode notmuch js-doc dired-du evil-snipe helm-dash web-beautify which-key doom-themes paradox restclient realgud evil-nerd-commenter eyebrowse emmet-mode emmet ox-reveal ox-impress-js org-impress-js ox-gfm org-brain company-ispell hl-todo imenu-anywhere pomidor company-yasnippet eclimd evil-lion git-gutter-fringe git-timemachine lice dired-launch auto-yasnippet quickrun evil-magit evil-god-state god-mode evil-args evil-visualstar evil-matchit evil-anzu evil-surround evil-leader powerline-evil powerline htmlize latex-extra tabbar rainbow-mode rainbow-delimiters org-bullets helm-swoop company-web company-jedi auto-package-update anaphora ac-html-bootstrap ac-dabbrev undo-tree)))
+    (keychain-environment company-tern tern kv a magit-todos yaml-mode docker dockerfile-mode moody minions multifiles define-word smart-jump highlight-thing speeddating use-package-ensure-system-package zerodark-theme ranger vlf vlfi org-super-agenda org-kanban org-edna gitattributes-mode gitconfig-mode dired-subtree dired-filter dired-ranger helm-google helm-gitignore helm-flx flx fzf ibuffer-vc helpful color-identifiers-mode darkroom org-notify org-alert toc-org org-projectile org-download diredful direful dired-hide-dotfiles evil-collection evil-colletion general company-statistics json-mode jedi virtualenv holiday holidays pdf-tools calfw-org calfw zenity-color-picker helm-system-packages exato evil-indent-plus evil-ediff evil-numbers evil-number evil-exchange ace-link git-messenger omnisharp omnisharp-emacs markdown-mode notmuch js-doc dired-du evil-snipe helm-dash web-beautify which-key doom-themes paradox restclient realgud evil-nerd-commenter eyebrowse emmet-mode emmet ox-reveal ox-impress-js org-impress-js ox-gfm org-brain company-ispell hl-todo imenu-anywhere pomidor company-yasnippet eclimd evil-lion git-gutter-fringe git-timemachine lice dired-launch auto-yasnippet quickrun evil-magit evil-god-state god-mode evil-args evil-visualstar evil-matchit evil-anzu evil-surround evil-leader powerline-evil powerline htmlize latex-extra tabbar rainbow-mode rainbow-delimiters org-bullets helm-swoop company-web company-jedi auto-package-update anaphora ac-html-bootstrap ac-dabbrev undo-tree)))
  '(palette-hex-rgb-digits 2)
  '(paradox-github-token t)
  '(powerline-gui-use-vcs-glyph nil)
@@ -147,8 +147,8 @@
     (cons 360 "#5B6268")))
  '(vc-annotate-very-old-color nil)
  '(web-mode-auto-close-style 2)
- '(web-mode-enable-current-column-highlight t t)
- '(web-mode-enable-current-element-highlight t t)
+ '(web-mode-enable-current-column-highlight t)
+ '(web-mode-enable-current-element-highlight t)
  '(which-key-hide-alt-key-translations nil)
  '(which-key-mode t))
 
