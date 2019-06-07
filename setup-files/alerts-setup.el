65b01adca9de347ab6c4b952585b6b6c525a878c
dchrzanowski <pjdamian.chrzanowski@gmail.com>
2019-06-07 22:48:21 +0100

    Brought the periodic timer back

--- setup-files/alerts-setup.el
+++ setup-files/alerts-setup.el
@@ -7,7 +7,37 @@
 ;; --------------------------------------------------------------------
 ;; alerts package
 ;; --------------------------------------------------------------------
-(use-package alert)
+(use-package alert
+  :config
+
+  ;; Reminder to stay healthy!
+  (defvar stay-healthy--start nil)
+  (defvar stay-healthy--timer nil)
+  (defun stay-healthy--alert (start-time)
+    "Alert the user to stay healthy"
+    (let* ((now (time-to-seconds (current-time)))
+           (seconds (- now start-time))
+           (hours (format-seconds "%h" seconds)))
+      (unless (string= hours "0")
+        (alert
+         (format "You've been working for %s hour(s)!" hours)
+         :title "Stay Healthy!"
+         ))))
+
+  (defun stay-healthy-start-timer ()
+    "Start a timer to keep you healthy"
+    (interactive)
+    (unless stay-healthy--start
+      (setq stay-healthy--start (time-to-seconds (current-time))))
+    (setq stay-healthy--timer (run-with-timer 0 (* 60 60) #'stay-healthy--alert stay-healthy--start)))
+
+  (defun stay-healthy-stop-timer ()
+    "End the healthy timer"
+    (interactive)
+    (when stay-healthy--timer
+      (cancel-timer stay-healthy--timer)))
+
+  (stay-healthy-start-timer))
 
 ;; --------------------------------------------------------------------
 ;; type-break mode
