ba01480f09f51cf2ae7cf6989ad43e91f6c0fb6f
dchrzanowski <pjdamian.chrzanowski@gmail.com>
2019-09-18 10:37:04 +0100

    Temp disable python lsp and re-enable elpy

--- setup-files/python-setup.el
+++ setup-files/python-setup.el
@@ -11,20 +11,20 @@
 ;; ;; --------------------------------------------------------------------
 ;; ;; Python
 ;; ;; --------------------------------------------------------------------
-;; (use-package elpy
-;;   :defer 4
-;;   :init
-;;   (use-package jedi)
-;;   :config
-;;   (progn
-;;     ;; Use Flycheck instead of Flymake
-;;     (when (require 'flycheck nil t)
-;;       (remove-hook 'elpy-modules 'elpy-module-flymake)
-;;       (remove-hook 'elpy-modules 'elpy-module-highlight-indentation)
-;;       (add-hook 'elpy-mode-hook 'flycheck-mode)
-;;       (add-hook 'elpy-mode-hook 'hl-todo-mode))
-;;     (elpy-enable)
-;;     (setq elpy-rpc-backend "jedi")))
+(use-package elpy
+  :defer 4
+  :init
+  (use-package jedi)
+  :config
+  (progn
+    ;; Use Flycheck instead of Flymake
+    (when (require 'flycheck nil t)
+      (remove-hook 'elpy-modules 'elpy-module-flymake)
+      (remove-hook 'elpy-modules 'elpy-module-highlight-indentation)
+      (add-hook 'elpy-mode-hook 'flycheck-mode)
+      (add-hook 'elpy-mode-hook 'hl-todo-mode))
+    (elpy-enable)
+    (setq elpy-rpc-backend "jedi")))
 
 (provide 'python-setup)
 ;;; python-setup ends here
