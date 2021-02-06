12a2848fd5118f7c89596e2b346824774a38f7c5
dchrzanowski <pjdamian.chrzanowski@gmail.com>
2021-02-06 21:11:24 +0000

    Added the flutter.el package

--- setup-files/dart-setup.el
+++ setup-files/dart-setup.el
@@ -5,5 +5,12 @@
 ;; --------------------------------------------------------------------
 (use-package dart-mode)
 
+(use-package flutter
+  :after dart-mode
+  :bind (:map dart-mode-map
+              ("C-M-x" . #'flutter-run-or-hot-reload))
+  :custom
+  (flutter-sdk-path "/opt/flutter/"))
+
 (provide 'dart-setup)
 ;;; dart-setup ends here
