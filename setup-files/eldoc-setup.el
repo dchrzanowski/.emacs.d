8aa9385467f78f796104d2d69922076711d0ca05
Damian Chrzanowski <pjdamian.chrzanowski@gmail.com>
2023-08-13 23:04:00 +0100

    Eldoc box is working properly again

--- setup-files/eldoc-setup.el
+++ setup-files/eldoc-setup.el
@@ -10,9 +10,9 @@
 (global-eldoc-mode)
 (setq eldoc-idle-delay 0.2)
 
-;; (use-package eldoc-box
-;;   :config
-;;   (add-hook 'prog-mode-hook 'eldoc-box-hover-mode))
+(use-package eldoc-box
+  :config
+  (add-hook 'prog-mode-hook 'eldoc-box-hover-mode))
 
 (provide 'eldoc-setup)
 ;;; eldoc-setup.el ends here
