e166cf5a069fd6264dc359e598e95752377bdb27
Damian Chrzanowski <pjdamian.chrzanowski@gmail.com>
2026-04-18 14:07:51 +0100

    Show breadcrumbs wherever possible

--- setup-files/project-assist-setup.el
+++ setup-files/project-assist-setup.el
@@ -20,5 +20,14 @@
         projectile-test-use-comint-mode t)
   (add-hook 'dired-mode-hook #'(lambda () (projectile-mode -1))))
 
+;; --------------------------------------------------------------------
+;; Breadcrumbs everywhere
+;; --------------------------------------------------------------------
+(use-package breadcrumb
+  :config
+  (breadcrumb-mode)
+  ;; exclude in org-mode. we have our own setup there
+  (add-hook 'org-mode-hook #'(lambda () (breadcrumb-local-mode -1))))
+
 (provide 'project-assist-setup)
 ;;; project-assist-setup.el ends here
